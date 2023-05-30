4f102124df563c92c3d046368067f7c01dbcbdfa
Sbrety <133106027+Sbrety@users.noreply.github.com>
2023-05-30 20:42:25 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://shorturl.at/otL48`
+					`https://shorturl.at/hmqzH`
 				}`,
 			},
 		};
